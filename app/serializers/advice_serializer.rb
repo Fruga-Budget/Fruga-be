6bc294ec846aaefff7fe677935df291c6da2633b
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-07-06 16:39:03 -0400

    Create advice_serializer

---
+++ app/serializers/advice_serializer.rb
@@ -0,0 +1,17 @@
+class AdviceSerializer
+  include JSONAPI::Serializer
+  
+  attributes :total_income, :expenses, :gpt_advice
+
+  attribute :expenses do |object|
+    {
+      needs: object.needs.map { |need| { name: need["name"], amount: need["cost"], description: need["description"], isNegotiable: need["isNegotiable"] } },
+      wants: object.wants.map { |want| { name: want["name"], amount: want["cost"], description: want["description"] } },
+      savings: object.savings.map { |saving| { name: saving["name"], amount: saving["cost"], description: saving["description"] } }
+    }
+  end
+
+  attribute :gpt_advice do |object|
+    object.recommendation.split('. ').map(&:strip)
+  end
+end
