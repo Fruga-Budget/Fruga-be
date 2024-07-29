62a977dfe4612ec16b0f2e9337813e38d70c0ba0
Grant Davis <grantdavis303@gmail.com>
2024-07-29 14:27:03 -0600

    Add Route for Advice Index

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create] do
-        resources :advices, only: [:create]
+        resources :advices, only: [:index, :create]
       end
       resources :sessions, only: [:create, :destroy]
     end
